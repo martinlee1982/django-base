d92bb7b179fb5f6b7ab2a063f624c45064957116
martin <caige225@sohu.com>
2022-04-27 22:58:12 +0800

    '创建模型'

--- bookmanager/book/models.py
+++ bookmanager/book/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    name = models.CharField(max_length=10)
+
+class People(models.Model):
+    name = models.CharField(max_length=10)
+    gender = models.BooleanField()
+    book = models.ForeignKey(Book,on_delete=models.CASCADE)
