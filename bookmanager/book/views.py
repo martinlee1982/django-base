ff9f7dd0ce61215f9bf7c9faea47727aece13125
martin <caige225@sohu.com>
2022-04-28 22:19:43 +0800

    '定义view里面的函数'

--- bookmanager/book/views.py
+++ bookmanager/book/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+'''
+视图
+所谓的视图，其实就是python函数
+视图函数有2个要求：
+1.视图函数的第一个参数就是接受请求
+2.必须返回一个相应
+'''
+from django.http import HttpRequest
+from django.http import HttpResponse
+def index(request):
+    return HttpResponse('ok')
+
